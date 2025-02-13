9967e3c96abf3256e2636c1e4f7d898e13ada1ee
BhargavGarge <bhargaavvv@gmail.com>
2025-02-14 01:33:41 +0530

    Initialize SocioApp with Routes and Nativewind

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function FeedScreen() {
+  return (
+    <View>
+      <Text className="text-5xl font-bold text-blue-500">Feed </Text>
+    </View>
+  );
+}
